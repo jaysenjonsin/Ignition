95db3a6211d62fb905af5049adb2847fe27a53c2
jaysenjonsin <jasoncjohnson5@yahoo.com>
2022-12-16 17:29:20 -0800

    image, css not rendering

--- webpack.common.js
+++ webpack.common.js
@@ -22,12 +22,20 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader'], // remember order matters --> goes from end to front
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'web/public/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
   ],
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,3 @@
-
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
@@ -6,13 +5,14 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    port: 8080,
+    port: 3005,
     hot: true,
     host: 'localhost',
     historyApiFallback: true,
     proxy: {
       '/api/*': {
-        target: 'http://localhost:3000',
+        //server is running on 5000
+        target: 'http://localhost:5000',
         secure: false,
       },
     },
