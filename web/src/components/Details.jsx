f93a072b09a48d0aff27213adc22b6b6e7b75685
jaysenjonsin <jasoncjohnson5@yahoo.com>
2022-12-20 17:53:39 -0800

    render additional button for MD or DO

--- web/src/components/Details.jsx
+++ web/src/components/Details.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import medication from '../images/medication.png';
 import pharmacy from '../images/pharmacy.png';
 
 const Details = ({ selectedTask, tasks }) => {
-  const { avatar } = useSelector((state) => state.auth);
+  const { user, avatar } = useSelector((state) => state.auth);
   return (
     <section className='details'>
       <div className='detailsContainer'>
@@ -21,13 +22,20 @@ const Details = ({ selectedTask, tasks }) => {
             {tasks.length === 0 ? '' : selectedTask?.medication}
           </div>
           <h2 style={{ paddingTop: '.5em' }}>Sent to</h2>
-          {/* <div className='prepare' style={{ display: 'flex' }}> */}
-          <div className='detailsCard'>
-            <img src={pharmacy} style={{ width: '4rem' }} alt='pharmacy' />
-            {tasks.length === 0 ? '' : selectedTask?.pharmacy}
+          <div style={{ display: 'flex' }}>
+            <div className='detailsCard'>
+              <img src={pharmacy} style={{ width: '4rem' }} alt='pharmacy' />
+              {tasks.length === 0 ? '' : selectedTask?.pharmacy}
+            </div>
+            <Link to='/' className='btn'>
+              Edit
+            </Link>
+            {(user?.role === 'MD' || user?.role === 'DO') && (
+              <Link to='/' className='btn'>
+                Additional Link
+              </Link>
+            )}
           </div>
-          {/* <div>hello</div> */}
-          {/* </div> */}
         </div>
       </div>
     </section>
